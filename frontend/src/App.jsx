d5b8995b38a62ad234e11cd528ba64fcd6470aec
sakshi8756 <sakshyada123@gmail.com>
2026-04-04 16:54:13 +0530

    Added react router dom (rounting) and create home page setup, sign in and signup setup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { Routes ,Route } from 'react-router-dom'
+import Login from './Pages/Login'
+import Home from './Pages/Home'
+import Signup from './Pages/Signup'
 
 const App = () => {
   return (
-    <div className='text-8xl'>
-      hello
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
+      
+    </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between bg-fuchsia-400 p-5 text-white'>
+      <Link to='/'>Project Management</Link>
+      <div className='flex gap-5'>
+        <Link to='/login'>Sign in</Link>
+        <Link to='/Signup'>Sign up</Link>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
